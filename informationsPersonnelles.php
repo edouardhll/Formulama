869816de12c075d13512557552fd6b1e582a0fe1
Edouard Hillion <edouardhll@gmail.com>
2026-03-31 12:38:18 +0200

    Update informationsPersonnelles.php

--- informationsPersonnelles.php
+++ informationsPersonnelles.php
@@ -5,7 +5,9 @@ $BDD = json_encode($_POST);
 
 $dataToJSON = $_POST;
 $dataToJSON['date'] = date('d/m/Y');
-
+$documentDate = $dataToJSON['documentDate'];
+$documentDate = date_create_from_format('Y-m-d', $documentDate)->format("d/m/Y");
+echo $documentDate;
 print_r($dataToJSON);
 echo "</pre>";
 // $fichier = fopen('bdd_'.date('Ymd').'.json', 'w+');
