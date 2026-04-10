32f3270d3c6ec443418d20054aa315fdd6d80452
Edouard Hillion <edouardhll@gmail.com>
2026-04-10 20:12:20 +0200

    100426

--- index.php
+++ index.php
@@ -2,145 +2,155 @@
 <head>
     <title>Formulama</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
+        Formulama
+    </header>
+    
+
     <form action="informationsPersonnelles.php" method="post">
 
 <!-- déclarant -->
+    <div class="separateur">
+        <h2>Déclarant</h2>
+        <div class="champFormulaire">
+            <label>Nom (acte de naissance)</label>
+            <input id="nom" type="text" value="<?php echo $_POST['nom']?>" placeholder="Dupont" name="nom">
+        </div>
 
-    <div class="champFormulaire">
-        <label>Nom (acte de naissance)</label>
-        <input id="nom" type="text" value="<?php echo $_POST['nom']?>" placeholder="Dupont" name="nom">
+        <div class="champFormulaire">
+            <label>Nom de famille</label>
+            <input id="nomUsage" type="text" value="" placeholder= "Dupont" name="nomUsage">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Prénom</label>
+            <input id="prenom" type="text" value="" placeholder= "Jean" name="prenom">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Date de naissance</label>
+            <input id="dateNaissance" type="date" value="" placeholder= "Date de naissance" name="dateNaissance">
+        </div>
+
+    <!-- RAJOUTER LE GENRE AVEC BALISE OPTION -->
+        <div class="champFormulaire">
+            <label>Civilité</label>
+            <select class="champFormulaire" id="sexe" name="sexe">
+                <option value="" disabled selected>Sélectionner</option>
+                <option>Homme</option>
+                <option>Femme</option>
+            </select>
+        </div>
+    </div>
+    <div class="separateur">
+        <div class="champFormulaire">
+            <label>Commune ou ville de naissance</label>
+            <input id="communeNaissance" type="text" value="" placeholder= "Nantes" name="communeNaissance">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Code postal de la ville de naissance</label>
+            <input id="codePostalNaissance" type="text" value="" placeholder= "44200" name="codePostalNaissance">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Pays de naissance</label>
+            <input id="paysNaissance" type="text" value="" placeholder= "France" name="paysNaissance">
+        </div>
     </div>
 
-    <div class="champFormulaire">
-        <label>Nom de famille</label>
-        <input id="nomUsage" type="text" value="" placeholder= "Dupont" name="nomUsage">
+    <!-- DOMICILE -->
+    <div class="separateur">
+        <div class="champFormulaire">
+            <label>Numéro de votre adresse</label>
+            <input id="numeroAdresse" type="text" value="" placeholder= "12" name="numeroAdresse">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Type de voie</label>
+            <select class="champFormulaire" id="typeVoieAdresse" name="typeVoieAdresse">
+                <option value="" disabled selected>Sélectionner</option>
+                <option>Rue</option>
+                <option>Avenue</option>
+                <option>Boulevard</option>
+                <option>Allée</option>
+                <option>Impasse</option>
+                <option>Place</option>
+                <option>Chemin</option>
+                <option>Route</option>
+            </select>
+        </div>
+
+        <div class="champFormulaire">
+            <label>Nom de la voie</label>
+            <input id="nomVoieAdresse" type="text" value="" placeholder= "Nom de la voie" name="nomVoieAdresse">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Code postal</label>
+            <input id="codePostal" type="text" value="" placeholder= "Code postal" name="codePostal">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Ville / Commune</label>
+            <input id="commune" type="text" value="" placeholder= "Votre ville ou commune" name="commune">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Pays</label>
+            <input id="pays" type="text" value="" placeholder= "Votre Pays" name="pays">
+        </div>
+    </div>
+    <!-- CARACTERISTIQUE DU TITRE -->
+    <div class="separateur">
+        <div class="champFormulaire">
+            <label>CNI / Passeport</label>
+            <select class="champFormulaire" id="perteIdentite" name="perteIdentite">
+                <option value="" disabled selected>Sélectionner</option>
+                <option>CNI</option>
+                <option>Passeport</option>
+            </select>
+        </div>
+
+        <div class="champFormulaire">
+            <label>N° document</label>
+            <input id="documentNumero" type="text" value="" placeholder= "T7X62TZ79" name="documentNumero">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Date de délivrance</label>
+            <input id="documentDate" type="date" placeholder= "" name="documentDate">
+        </div>
     </div>
 
-    <div class="champFormulaire">
-        <label>Prénom</label>
-        <input id="prenom" type="text" value="" placeholder= "Jean" name="prenom">
+    <!-- CIRCONSTANCES DE LA PERTE -->
+    <div class="separateur">
+        <div class="champFormulaire">
+            <label>Date de perte</label>
+            <input id="datePerte" type="date" name="datePerte">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Lieu de perte</label>
+            <input id="lieuPerte" type="text" placeholder="Gare de nantes" name="lieuPerte">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Circonstances de la perte</label>
+            <input id="circonstancesPerte" type="text" value="" placeholder= "Mon sac est resté ouvert..." name="circonstancesPerte">
+        </div>
+
+        <div class="champFormulaire">
+            <label>Délivré par</label>
+            <input id="delivrePar" type="text" value="" placeholder= "Préfecture de la Loire-Atlantique" name="delivrePar">
+        </div>
+    </div>    
+    <div class="separateur">
+            <input type="submit">
+            <input type="reset">
+        </form>
     </div>
-
-    <div class="champFormulaire">
-        <label>Date de naissance</label>
-        <input id="dateNaissance" type="date" value="" placeholder= "Date de naissance" name="dateNaissance">
-    </div>
-
-<!-- RAJOUTER LE GENRE AVEC BALISE OPTION -->
-    <div class="champFormulaire">
-        <label>Civilité</label>
-        <select class="champFormulaire" id="sexe" name="sexe">
-            <option value="" disabled selected>Sélectionner</option>
-            <option>Homme</option>
-            <option>Femme</option>
-        </select>
-    </div>
-
-    <div class="champFormulaire">
-        <label>Commune ou ville de naissance</label>
-        <input id="communeNaissance" type="text" value="" placeholder= "Nantes" name="communeNaissance">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Code postal de la ville de naissance</label>
-        <input id="codePostalNaissance" type="text" value="" placeholder= "44200" name="codePostalNaissance">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Pays de naissance</label>
-        <input id="paysNaissance" type="text" value="" placeholder= "France" name="paysNaissance">
-    </div>
-
-
-<!-- DOMICILE -->
-
-    <div class="champFormulaire">
-        <label>Numéro de votre adresse</label>
-        <input id="numeroAdresse" type="text" value="" placeholder= "12" name="numeroAdresse">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Type de voie</label>
-        <select class="champFormulaire" id="typeVoieAdresse" name="typeVoieAdresse">
-            <option value="" disabled selected>Sélectionner</option>
-            <option>Rue</option>
-            <option>Avenue</option>
-            <option>Boulevard</option>
-            <option>Allée</option>
-            <option>Impasse</option>
-            <option>Place</option>
-            <option>Chemin</option>
-            <option>Route</option>
-        </select>
-    </div>
-
-    <div class="champFormulaire">
-        <label>Nom de la voie</label>
-        <input id="nomVoieAdresse" type="text" value="" placeholder= "Nom de la voie" name="nomVoieAdresse">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Code postal</label>
-        <input id="codePostal" type="text" value="" placeholder= "Code postal" name="codePostal">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Ville / Commune</label>
-        <input id="commune" type="text" value="" placeholder= "Votre ville ou commune" name="commune">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Pays</label>
-        <input id="pays" type="text" value="" placeholder= "Votre Pays" name="pays">
-    </div>
-
-<!-- CARACTERISTIQUE DU TITRE -->
-
-    <div class="champFormulaire">
-        <label>CNI / Passeport</label>
-        <select class="champFormulaire" id="perteIdentite" name="perteIdentite">
-            <option value="" disabled selected>Sélectionner</option>
-            <option>CNI</option>
-            <option>Passeport</option>
-        </select>
-    </div>
-
-    <div class="champFormulaire">
-        <label>N° document</label>
-        <input id="documentNumero" type="text" value="" placeholder= "T7X62TZ79" name="documentNumero">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Date de délivrance</label>
-        <input id="documentDate" type="date" placeholder= "" name="documentDate">
-    </div>
-
-
-<!-- CIRCONSTANCES DE LA PERTE -->
-
-    <div class="champFormulaire">
-        <label>Date de perte</label>
-        <input id="datePerte" type="date" name="datePerte">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Lieu de perte</label>
-        <input id="lieuPerte" type="text" placeholder="Gare de nantes" name="lieuPerte">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Circonstances de la perte</label>
-        <input id="circonstancesPerte" type="text" value="" placeholder= "Mon sac est resté ouvert..." name="circonstancesPerte">
-    </div>
-
-    <div class="champFormulaire">
-        <label>Délivré par</label>
-        <input id="delivrePar" type="text" value="" placeholder= "Préfecture de la Loire-Atlantique" name="delivrePar">
-    </div>
-
-        <input type="submit">
-        <input type="reset">
-    </form>
 </body>
--- lectureDocument/lectureDoc.py
+++ lectureDocument/lectureDoc.py
@@ -1,7 +1,9 @@
 import easyocr
+#import pillow
+#resize()
 reader = easyocr.Reader(['fr','en'])
 result = reader.readtext('lectureDocument/carteIdentite.jpg')
-# result2= reader.readtext('lectureDocument/carteIdentite2.jpg')
+result2= reader.readtext('lectureDocument/carteIdentite2.jpg')
 
 # #============== ORDRE INFOS ==============#
 
@@ -10,10 +12,69 @@ result = reader.readtext('lectureDocument/carteIdentite.jpg')
 # # COIN BAS DROIT
 # # COIN BAS GAUCHE
 
-for i in range(len(result)):
-     print(result[i][1])
-print(result)
+#for i in range(len(result)):
+#     print(result[i][1])
+#print(f'CNI 1 {len(result)}')
+      
+for i in range(len(result2)):
+     print(result2[i][1])
+#print(f'CNI2 {len((result))}')
+
+
+cni = {"nom": result2[5][1],
+       "prenom": result2[8][1],
+       "sexe": result2[16][1],
+       "nationnalite": result2[14][1],
+       "dateNaissance": result2[17][1],
+       "lieuNaissance": result2[19][1],
+       }
+
+
+if result2[9][1] != "SEXE / Sex":
+     cni["prenom"]+= " "+result2[9][1]
+
+if result2[10][1] != "SEXE / Sex":
+     cni["prenom"]+= " "+result2[10][1]
+
+print(f'CNI2 {(cni)}')
+
 
 
 with open('lectureDocument/cni.txt', 'w', encoding='utf-8') as f:
     f.write(str(result))
+
+
+#CHERCHER SI ON PEUT FILTRER PAR TYPO
+#IMPRIMER PLUSIEURS CNI POUR LES PRENDRE EN PHOTO AVEC DES ECLAIRAGES DIFFERENTS
+
+#RÉPUBLIQUE FRANÇAISE
+# FR
+# CARTE NATIONALE D'IDENTITÉ
+# IDENTITY CARD
+# NOM /Sumname
+# CHEVALLIER
+# Prenoms / Given names
+# Gisèle , Audrey
+# SEXE /Sex
+# NATIONALITE
+# Nationality
+# DATE DE NAISS
+# Dale ofbrth
+# E
+# FRA
+# 01 04 1995
+# LIEU DE NAISSANCE
+# Place of birth
+# BORDEAUX
+# NOM D'USAGE / Altemate name
+# vve
+# DUBOIS
+# N' DU DOCUMENT / Document No
+# DATED'EXPIR
+# Expiry dale
+# T7X62Tz79
+# 27 01 2031
+# JignAture
+# 240220
+# RECETIE
+
